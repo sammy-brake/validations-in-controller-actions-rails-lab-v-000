257582960f43e2c7c5d9bc2952e91c03e78e3b15
Samantha brake <sammy.brake@gmail.com>
2019-04-06 05:10:07 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,7 +7,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.new(author_params)
+    @author = Author.new!(author_params)
     if @author.valid?
       redirect_to author_path(@author)
     else 
