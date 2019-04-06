c221b3f6fc40ed5edf74c87dc252f590d89c3e7e
Samantha brake <sammy.brake@gmail.com>
2019-04-06 05:17:02 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,11 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.update(post_params).valid?
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else 
       render :edit 
-  end 
+    end 
   end
 
   private
