d7d1c37f8d9a896534de44de9e3863336865dcc4
Samantha brake <sammy.brake@gmail.com>
2019-04-06 05:08:10 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,11 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    if @author = Author.create!(author_params)
+      redirect_to author_path(@author)
+    else 
+      render :new
+    end 
   end
 
   private
